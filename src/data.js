08808a5e48662631f83c62b8afc0ed3d1d2161ac
Gilad <giladaya@gmail.com>
2017-01-22 12:17:04 +0200

    add some more drawings

--- src/data.js
+++ src/data.js
@@ -48,6 +48,57 @@ export default [
     ]
   },
   {
+    title: "Bull",
+    thumbnail: 'images/bull_07.png',
+    steps: [
+      'images/bull_01.png',
+      'images/bull_02.png',
+      'images/bull_03.png',
+      'images/bull_04.png',
+      'images/bull_05.png',
+      'images/bull_06.png',
+      'images/bull_07.png',
+    ]
+  },
+  {
+    title: "Gold Fish",
+    thumbnail: 'images/goldfish_07.png',
+    steps: [
+      'images/goldfish_01.png',
+      'images/goldfish_02.png',
+      'images/goldfish_03.png',
+      'images/goldfish_04.png',
+      'images/goldfish_05.png',
+      'images/goldfish_06.png',
+      'images/goldfish_07.png',
+    ]
+  },
+  {
+    title: "Sleeping Cat",
+    thumbnail: 'images/sleep-cat_07.png',
+    steps: [
+      'images/sleep-cat_01.png',
+      'images/sleep-cat_02.png',
+      'images/sleep-cat_03.png',
+      'images/sleep-cat_04.png',
+      'images/sleep-cat_05.png',
+      'images/sleep-cat_06.png',
+      'images/sleep-cat_07.png',
+    ]
+  },
+  {
+    title: "Turtle",
+    thumbnail: 'images/turtle_06.png',
+    steps: [
+      'images/turtle_01.png',
+      'images/turtle_02.png',
+      'images/turtle_03.png',
+      'images/turtle_04.png',
+      'images/turtle_05.png',
+      'images/turtle_06.png',
+    ]
+  },
+  {
     title: "Letters",
     thumbnail: 'images/letters_A.gif',
     steps: [
