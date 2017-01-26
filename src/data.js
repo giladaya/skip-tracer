ff3e28246b5ff03429877359ae81fb8578411622
Gilad <giladaya@gmail.com>
2017-01-26 14:12:28 +0200

    focus

--- src/data.js
+++ src/data.js
@@ -1,40 +1,5 @@
 export default [
   {
-    title: "Puppy",
-    thumbnail: 'images/puppy_06.png',
-    steps: [
-      'images/puppy_01.png',
-      'images/puppy_02.png',
-      'images/puppy_03.png',
-      'images/puppy_04.png',
-      'images/puppy_05.png',
-      'images/puppy_06.png',
-    ]
-  },
-  {
-    title: "Whale",
-    thumbnail: 'images/whale_05.png',
-    steps: [
-      'images/whale_01.png',
-      'images/whale_02.png',
-      'images/whale_03.png',
-      'images/whale_04.png',
-      'images/whale_05.png',
-    ]
-  },
-  {
-    title: "Shark",
-    thumbnail: 'images/shark_06.png',
-    steps: [
-      'images/shark_01.png',
-      'images/shark_02.png',
-      'images/shark_03.png',
-      'images/shark_04.png',
-      'images/shark_05.png',
-      'images/shark_06.png',
-    ]
-  },
-  {
     title: "Bear",
     thumbnail: 'images/bear_07.png',
     steps: [
@@ -99,35 +64,15 @@ export default [
     ]
   },
   {
-    title: "Letters",
-    thumbnail: 'images/letters_A.gif',
+    title: "Puppy",
+    thumbnail: 'images/puppy_06.png',
     steps: [
-      'images/letters_A.gif',
-      'images/letters_B.gif',
-      'images/letters_C.gif',
-      'images/letters_D.gif',
-      'images/letters_E.gif',
-      'images/letters_F.gif',
-      'images/letters_G.gif',
-      'images/letters_H.gif',
-      'images/letters_I.gif',
-      'images/letters_J.gif',
-      'images/letters_K.gif',
-      'images/letters_L.gif',
-      'images/letters_M.gif',
-      'images/letters_N.gif',
-      'images/letters_O.gif',
-      'images/letters_P.gif',
-      'images/letters_Q.gif',
-      'images/letters_R.gif',
-      'images/letters_S.gif',
-      'images/letters_T.gif',
-      'images/letters_U.gif',
-      'images/letters_V.gif',
-      'images/letters_W.gif',
-      'images/letters_X.gif',
-      'images/letters_Y.gif',
-      'images/letters_Z.gif',
+      'images/puppy_01.png',
+      'images/puppy_02.png',
+      'images/puppy_03.png',
+      'images/puppy_04.png',
+      'images/puppy_05.png',
+      'images/puppy_06.png',
     ]
   },
 ]
